6b92ad16ac92c24138bb4bebd54dd50c5aef4eec
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-06-13 16:48:52 +0100

    Update git-setup.sh
    Added sudo to CD and chmod

--- git-setup.sh
+++ git-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y install git
-cd /root/
+sudo cd /root/
 sudo git clone https://github.com/kevwong16/wordpress-project.git
-chmod -R 755 wordpress-project
+sudo chmod -R 755 wordpress-project
 sudo bash wordpress-project/lemp-setup.sh
