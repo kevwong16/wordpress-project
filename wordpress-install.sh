8beb9c5e6387e85fb4a308f60523e3f912afb575
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-06-19 15:20:48 +0100

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cd /var/www/html
+sudo cd /var/www/html/
 sudo apt -y install unzip
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
@@ -11,9 +11,16 @@ sudo mariadb -u root
 # flush privileges;
 # exit;
 # wget wp-config.php from s3bucket
+sudo cd wordpress/
+sudo cp wp-config-sample.php wp-config.php
+sudo vi wp-config.php
+# add db name, username and password
 sudo chmod 640 wp-config.php
 sudo chown www-data:www-data /var/www/html/wordpress/ -R
 sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
+sudo vi /etc/nginx/conf.d/wordpress.conf
+# server_name ec2-50-19-145-199.compute-1.amazonaws.com;
+# root /var/www/html/wordpress/;
 sudo nginx -t
 sudo systemctl reload nginx
