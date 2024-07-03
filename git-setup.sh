32472d049ae0449fc69d4e40d3c2eaf91c170d2b
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-03 12:08:08 +0100

    Update git-setup.sh
    Added update and upgrade to the script

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # sudo apt -y install git
+sudo apt -y update
+sudo apt -y upgrade
 sudo git clone https://github.com/kevwong16/wordpress-project.git /root/wordpress-project
 sudo chmod -R 755 /root/wordpress-project
 sudo bash /root/wordpress-project/lemp-setup.sh
