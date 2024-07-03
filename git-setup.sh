a8a78eeb6ea50321ee753914b59170f40519e611
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-03 10:35:27 +0100

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt -y install git # installs git
-sudo git clone https://github.com/kevwong16/wordpress-project.git /root # clones git repository from the url into root directory
-sudo chmod -R 755 wordpress-project # changes permissions of the directory and all its contents. The `755` permission grants read, write, and execute permissions to the owner and read and execute permissions to group and others
-sudo bash wordpress-project/lemp-setup.sh # runs the lemp-setup.sh script located in the wordpress-project directory with Bash
+# sudo apt -y install git
+sudo git clone https://github.com/kevwong16/wordpress-project.git /root/wordpress-project
+sudo chmod -R 755 /root/wordpress-project
+sudo bash /root/wordpress-project/lemp-setup.sh
