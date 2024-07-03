badf0cb98542425fa1f0a6c0f6a1b934027b4efc
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-03 14:13:48 +0100

    Update lemp-setup.sh
    added dns_record variable and updates the nginx.conf

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -9,4 +9,7 @@ systemctl status mariadb >> /root/testing.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/testing.txt
 sudo mv /var/www/html/index.html /var/www/html/index.html.old # rename apache testing page
+sudo mv /root/wordpress-project/nginx.conf /etc/nginx/conf.d/nginx.conf
+dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')
+sed -i "s/SERVERNAME/$dns_record/g" /etc/nginx/conf.d/nginx.conf
 sudo bash /root/wordpress-project/wordpress-install.sh
