cb335eefb16b35d496e1634e13636b16a001d18f
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-03 12:08:29 +0100

    Update lemp-setup.sh
    Removed update and upgrade from script

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt -y update
-sudo apt -y upgrade
 sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
 sudo apt -y install nginx
 sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
