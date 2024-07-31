46aff60b8be2348ac99cf8174903b3c280b98169
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-31 14:17:00 +0100

    Update lemp-setup.sh
    changed dns_record to kevwong.uk

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -14,7 +14,7 @@ sudo systemctl disable apache2 # disables apache from starting on a server reboo
 # sudo apt remove --purge apache2
 sudo mv /var/www/html/index.html /var/www/html/index.html.old # rename apache testing page
 sudo mv /root/wordpress-project/nginx.conf /etc/nginx/conf.d/nginx.conf
-dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')
+dns_record="kevwong.uk"
 sed -i "s/SERVERNAME/$dns_record/g" /etc/nginx/conf.d/nginx.conf
 nginx -t && systemctl reload nginx # this will only reload nginx if the test is successful
 sudo bash /root/wordpress-project/certbot-ssl-install.sh
