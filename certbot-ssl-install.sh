7748da5612b97559ad123dceab0cb200c67e0cf2
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-07-31 15:18:15 +0100

    Update certbot-ssl-install.sh
    added domain in

--- certbot-ssl-install.sh
+++ certbot-ssl-install.sh
@@ -8,12 +8,14 @@ sudo apt install -y certbot python3-certbot-nginx
 # Define your email
 EMAIL="kevin.wong@apprentice.ua92.ac.uk"
 
-# Use Certbot to obtain and install the SSL certificate
-sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL
+# Define your domain(s)
+DOMAIN="wp.kevwong.uk"
+
+# Use Certbot to obtain and install the SSL certificate for the specified domain
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
 
 # Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
-sudo nginx -t && systemctl reload nginx
+sudo nginx -t && sudo systemctl reload nginx
 
-# Run the WordPress installation scrip
-
+# Run the WordPress installation script
 sudo bash /root/wordpress-project/wordpress-install.sh
