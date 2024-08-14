f0518eed766ea63b114fe727cc454a05c2d28154
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-08-14 15:13:55 +0100

    Update wordpress-install.sh
    added line to generate random password and include it in wordpress database

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -6,12 +6,19 @@ sudo unzip /var/www/latest.zip -d /var/www/
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html 
 # sudo mariadb -u root
+
+# generates a random password for use in WP DB
+password=$(tr -dc 'A-Za-z0-9!' < /dev/urandom | head -c 25)
+
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by 'theclassof92'"
+sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by '$password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
 sudo wget -O /var/www/html/wp-config.php https://keffin-bucket.s3.amazonaws.com/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/
+
+sed -i 's/password_here/$password/g' wp-config.php
+
 # sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
