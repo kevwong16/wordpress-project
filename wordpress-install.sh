bacda6c1127400928d9e3ae6ecc624b400125183
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-08-21 14:55:04 +0100

    Update wordpress-install.sh
    added lines to generate random username and SALT values

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -19,22 +19,30 @@ sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html 
 
 # Generate a random password for the WordPress database user using a mix of alphanumeric characters and symbols.
-password=$(tr -dc 'A-Za-z0-9!' < /dev/urandom | head -c 25)
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
+
 
 # Create a new MySQL database named 'wordpress' if it doesn't already exist.
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 
 # Create a new MySQL user 'wpuser' with the generated password if the user doesn't already exist.
-sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by '$password'"
+sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$password'"
 
 # Grant all privileges on the 'wordpress' database to the 'wpuser' user.
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
 
 # Apply the changes made to the MySQL privileges.
 sudo mysql -e "FLUSH PRIVILEGES"
 
 # Download a pre-configured wp-config.php file from an S3 bucket and save it to /var/www/html/.
-sudo wget -O /var/www/html/wp-config.php https://keffin-bucket.s3.amazonaws.com/wp-config.php
+# sudo wget -O /var/www/html/wp-config.php https://keffin-bucket.s3.amazonaws.com/wp-config.php
+
+sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 # Set permissions for the wp-config.php file to be readable and writable by the owner and readable by the group.
 sudo chmod 640 /var/www/html/wp-config.php 
@@ -44,6 +52,12 @@ sudo chown -R www-data:www-data /var/www/html/
 
 # Replace the placeholder 'password_here' in wp-config.php with the generated password.
 sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
+
+SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+STRING='put your unique phrase here'
+printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
 
 # (Optional) Navigate to the /etc/nginx/conf.d/ directory and create a new Nginx configuration file for WordPress.
 # sudo cd /etc/nginx/conf.d/
